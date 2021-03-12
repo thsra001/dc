43963be390700e4f9deff5101512757a9086c6ed
thsra001 <44636308+thsra001@users.noreply.github.com>
2021-03-12 18:19:58 +0100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,28 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    mySprite.vy = -30
 })
-let mySprite = sprites.create(img`
+let mySprite: Sprite = null
+tiles.setTilemap(tilemap`level6`)
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . 8 8 8 8 8 . . . . . . . . 
-    . . . 8 8 8 8 8 8 8 8 8 . . . . 
-    . . . 8 8 8 8 8 8 8 8 8 8 8 . . 
+    . . . c c c c c . . . . . . . . 
+    . . . c f 8 8 8 9 9 9 9 . . . . 
+    . . . c f 8 8 8 8 8 8 8 8 8 . . 
     . . . . 8 8 8 8 8 8 8 8 8 8 . . 
     . . . . 8 8 8 8 8 8 8 8 8 8 . . 
-    . . . . 8 8 8 8 8 8 8 8 8 . . . 
-    . . . . 8 8 . . . 8 8 . . . . . 
+    . . . . f f 8 8 8 f f 8 8 . . . 
+    . . . . f f . . . f f . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-mySprite.vy = 30
+mySprite.ay = 30
 scene.cameraFollowSprite(mySprite)
+forever(function () {
+    mySprite.vx = 111
+})
